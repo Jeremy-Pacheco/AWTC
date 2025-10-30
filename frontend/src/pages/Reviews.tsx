082dfb2ec09645ba6848f37def0bd531d6e7a734
Jeremy-Pacheco <183743767+Jeremy-Pacheco@users.noreply.github.com>
2025-10-30 16:24:43 +0000

    feat: update backend and frontend

---
+++ frontend/src/pages/Reviews.tsx
@@ -0,0 +1,9 @@
+function Reviews() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Reviews</h1>
+    </>
+  );
+}
+
+export default Reviews;
---
+++ frontend/src/pages/Volunteering.tsx
@@ -0,0 +1,11 @@
+
+
+function Volunteering() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Volunteering</h1>
+    </>
+  );
+}
+
+export default Volunteering;
