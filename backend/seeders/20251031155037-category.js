5c946c137be5b2496f077fdb1c84292f0e01d8a5
emilygarrui <emilygarciaruiz@alumno.ieselrincon.es>
2025-10-31 15:56:36 +0000

    feat: crud-category

---
+++ backend/seeders/20251031155037-category.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "Categories",
+      [
+        {
+          name: "Social",
+          description: "For projects based on social aid",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("People", null, {});
+  },
+};
