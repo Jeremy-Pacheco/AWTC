2f88600ec91a8d1876953357bb74930954bdd3be
ElMasue <ssamuelcorado@gmail.com>
2025-11-02 17:50:50 +0100

    fix:Deploy Frontend-13

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,12 @@
-// frontend/index.js
-const express = require('express');
-const path = require('path');
-const app = express();
+// index.js (ESM)
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
 const PORT = process.env.PORT || 3000;
 const frontendPath = path.join(__dirname, 'dist');
 
