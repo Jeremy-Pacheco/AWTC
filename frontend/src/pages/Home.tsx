fb60e3dee2725cb67bbdeb7dc5050fd72a146fd6
Jeremy-Pacheco <pachechoonajeremypaul@gmail.com>
2025-11-02 22:59:26 +0000

    fix: font family

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,9 +5,6 @@ function Home() {
     <>
     {/* <NavBar /> */}
       <h1 className="Display">A will to change</h1>
-      <h2>dfgsdfsdf</h2>
-      <h3>Empoderando a las mujeres en la tecnología</h3>
-      <h4>Un espacio para el crecimiento y la colaboración</h4>
     </>
   );
 }
