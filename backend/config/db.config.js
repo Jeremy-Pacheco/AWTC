c4c2167e63c99868e680616529b1db507ab7bed3
Jeremy-Pacheco <183743767+Jeremy-Pacheco@users.noreply.github.com>
2025-11-14 11:54:44 +0000

    feat:connection with the database

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,13 +1,34 @@
-module.exports = {
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: '1234',
-    DB: 'db_awtc',
-    dialect: 'mysql',
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+const path = require("path");
+const dotenv = require("dotenv");
+
+const env = process.env.NODE_ENV;
+const envPath = path.resolve(__dirname, "..", `.env.${env}`);
+
+dotenv.config({ path: envPath });
+
+const config = {
+development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: "mysql",
+    dialectOptions: {
+    ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
+    },
+},
+production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: "mysql",
+    dialectOptions: {
+    ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
+    },
+},
 };
+
+module.exports = config[env];
