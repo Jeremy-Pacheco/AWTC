9a455f15467edfb3afd5b4c1fcfa87e8f530c3ad
emilygarrui <emilygarciaruiz@alumno.ieselrincon.es>
2025-11-27 16:35:06 +0000

    fix: deploy errors and fixing CORS

--- backend/config/initAdmin.js
+++ backend/config/initAdmin.js
@@ -2,17 +2,21 @@ const bcrypt = require('bcrypt');
 const { User } = require('../models');
 
 async function initAdmin() {
-  const existingAdmin = await User.findOne({ where: { role: 'admin' } });
-  if (!existingAdmin) {
-    await User.create({
-        name: 'Admin',
-        email: 'admin@awtc.es',
-        password: 'adminawtc1234',
-        role: 'admin'
-    });
-    console.log('Admin account created: admin@awtc.es / adminawtc1234');
-  } else {
-    console.log('Admin already exists');
+  try {
+    const existingAdmin = await User.findOne({ where: { role: 'admin' } });
+    if (!existingAdmin) {
+      await User.create({
+          name: 'Admin',
+          email: 'admin@awtc.es',
+          password: 'adminawtc1234',
+          role: 'admin'
+      });
+      console.log('Admin account created: admin@awtc.es / adminawtc1234');
+    } else {
+      console.log('Admin already exists');
+    }
+  } catch (error) {
+    console.error('Error creating admin:', error.message);
   }
 }
 
