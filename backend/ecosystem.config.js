10d1c1caa48235aa87992de8570026dbc39368cb
ElMasue <ssamuelcorado@gmail.com>
2025-12-03 20:42:09 +0100

    feat: added dns instalation and service configuration

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  apps: [
+    {
+      name: "awtc-backend",
+      script: "index.js",
+      env: {
+        NODE_ENV: "production",
+        PORT: 8080
+      },
+      instances: 1,
+      exec_mode: "fork",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G"
+    },
+    {
+      name: "awtc-dns",
+      script: "dns-server/dns-server.js",
+      env: {
+        DNS_PORT: 5454
+      },
+      instances: 1,
+      exec_mode: "fork",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "512M"
+    }
+  ]
+};
